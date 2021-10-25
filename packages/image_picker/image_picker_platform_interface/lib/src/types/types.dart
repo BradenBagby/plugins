1e9c1550a43641529def35402a255383eb2d6a94
BradenBagby <braden.bagby@wavv.com>
2021-10-25 16:06:13 -0500

    exports video_export_quality.dart in types

--- packages/image_picker/image_picker_platform_interface/lib/src/types/types.dart
+++ packages/image_picker/image_picker_platform_interface/lib/src/types/types.dart
@@ -7,6 +7,7 @@ export 'image_source.dart';
 export 'retrieve_type.dart';
 export 'picked_file/picked_file.dart';
 export 'lost_data_response.dart';
+export 'video_export_quality.dart';
 
 /// Denotes that an image is being picked.
 const String kTypeImage = 'image';
